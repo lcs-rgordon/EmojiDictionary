2169b8b22df387f68c84fc4ab80b7ab398c4d7a1
Russell Gordon <github@russellgordon.ca>
2019-11-13 17:21:54 -0500

    Basic navigation from table view to detail view is working.

---
+++ EmojiDictionary/EmojiDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiDetailViewController.swift
+//  EmojiDictionary
+//
+//  Created by Gordon, Russell on 2019-11-13.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import UIKit
+
+class EmojiDetailViewController: UIViewController {
+
+    // MARK: Properties
+    var emojiToDisplay: Emoji = Emoji()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Now set up the view to display information about the selected emoji
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
