a3639a3a9767ae7f1572fa1464a3642a2b58f86e
Russell Gordon <github@russellgordon.ca>
2019-11-13 17:59:54 -0500

    Configured visual appearance of detail view.

--- EmojiDictionary/EmojiDetailViewController.swift
+++ EmojiDictionary/EmojiDetailViewController.swift
@@ -11,19 +11,36 @@ import UIKit
 class EmojiDetailViewController: UIViewController {
 
     // MARK: Properties
+    // The emoji to display detail about
     var emojiToDisplay: Emoji?
     
+    // Outlets to connect this controller to the view
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var usageLabel: UILabel!
+    
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the navigation item title
-        self.navigationItem.title = emojiToDisplay!.name
+        // Get a reference to the emoji set in the table view prior to segue
+        guard let emojiSetFromTableView = emojiToDisplay else {
+            return
+        }
+        
+        // Set the navigation title item
+        self.navigationItem.title = emojiSetFromTableView.name
+        
+        // Display detail information in the view
+        symbolLabel.text = emojiSetFromTableView.symbol
+        descriptionLabel.text = emojiSetFromTableView.description
+        usageLabel.text = emojiSetFromTableView.usage
 
         // Now set up the view to display information about the selected emoji
-        print("Name is: \(emojiToDisplay!.name)")
-        print("Symbol is: \(emojiToDisplay!.symbol)")
-        print("Description is: \(emojiToDisplay!.description)")
-        print("Usage is: \(emojiToDisplay!.usage)")
+        print("Name is: \(emojiSetFromTableView.name)")
+        print("Symbol is: \(emojiSetFromTableView.symbol)")
+        print("Description is: \(emojiSetFromTableView.description)")
+        print("Usage is: \(emojiSetFromTableView.usage)")
 
     }
 
@@ -36,5 +53,7 @@ class EmojiDetailViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
 }
