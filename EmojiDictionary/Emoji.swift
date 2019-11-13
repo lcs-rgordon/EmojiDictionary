93eb09d88635fd3b811765e63c31541ee9c09f36
Russell Gordon <github@russellgordon.ca>
2019-11-13 14:51:55 -0500

    Added the model.

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Gordon, Russell on 2019-11-13.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
